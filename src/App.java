e728062b063141c9a2ed9e7be1e4cac09a5cc432
Sangeeta Bose <sangeeta.bose.236@gmail.com>
2025-08-21 14:26:46 +0530

    Solution

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+        System.out.println(isPalindrome(1234));
+    }
+
+        public static boolean isPalindrome(int number){
+            int reverse = 0;
+            int original = number;
+            while(original != 0){
+                reverse = reverse * 10 + original % 10;
+                original = original / 10;
+            }
+            return reverse == number;
+        }
+}
